825862f5ab356add1a13baf0203d10c5ffeab702
yukimasaki <woodenhoe@gmail.com>
2023-11-02 17:03:51 +0900

    feat: Keycloakへのリダイレクト処理を実装

--- sample-app/nuxt3/sample-app/app.vue
+++ sample-app/nuxt3/sample-app/app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <button
+      @click="login()"
+    >
+      Login
+    </button>
   </div>
 </template>
+
+<script setup lang="ts">
+import Keycloak from 'keycloak-js';
+
+const nuxtApp = useNuxtApp();
+
+const login = () => {
+  // console.log(nuxtApp);
+  // console.log('----> ', nuxtApp.$keycloak);
+  // (nuxtApp.$keycloak as Keycloak).login();
+}
+</script>
