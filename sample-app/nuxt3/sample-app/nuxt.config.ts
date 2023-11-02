66550c448437c4248dcc7272083c80beaa958bb6
yukimasaki <woodenhoe@gmail.com>
2023-11-02 17:03:25 +0900

    feat: 環境変数(.env)をruntimeConfig経由で読み込み

--- sample-app/nuxt3/sample-app/nuxt.config.ts
+++ sample-app/nuxt3/sample-app/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  runtimeConfig: {
+    public: {
+      keycloakAuthServerUrl: process.env.KEYCLOAK_AUTH_SERVER_URL,
+      keycloakRealm: process.env.KEYCLOAK_REALM,
+      keycloakClientId: process.env.KEYCLOAK_CLIENT_ID,
+    },
+  },
+  devtools: { enabled: true },
 })
