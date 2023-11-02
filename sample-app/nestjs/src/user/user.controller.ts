40fd3657deef12bea3885e65dd16389d40bda934
yukimasaki <woodenhoe@gmail.com>
2023-11-02 12:13:52 +0900

    feat: API保護用のデコレータを付与

--- sample-app/nestjs/src/user/user.controller.ts
+++ sample-app/nestjs/src/user/user.controller.ts
@@ -1,21 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Roles, Unprotected } from 'nest-keycloak-connect';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @Unprotected()
   getpublic(): string {
     return `${this.userService.getHello()} from public`;
   }
 
   @Get('/user')
+  @Roles({ roles: ['user']})
   getUser(): string {
     return `${this.userService.getHello()} from user`;
   }
 
   @Get('/admin')
+  @Roles({ roles: ['admin']})
   getAdmin(): string {
     return `${this.userService.getHello()} from admin`;
   }
