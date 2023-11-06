0f6d75aface341aae5623a9ec227dd20e41558ad
yukimasaki <woodenhoe@gmail.com>
2023-11-06 13:13:44 +0900

    feat: API仕様書の自動作成を目的としてSwaggerを導入

--- sample-app/nestjs/sample-app/src/main.ts
+++ sample-app/nestjs/sample-app/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger設定
+  const swaggerConfig = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
