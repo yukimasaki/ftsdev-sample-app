ef2f2e3c156e09373b59ab6d08d953b2873bf6cc
yukimasaki <woodenhoe@gmail.com>
2023-11-06 13:53:58 +0900

    feat: UserEntity作成・prisma migrate

--- sample-app/nestjs/sample-app/src/users/entities/user.entity.ts
+++ sample-app/nestjs/sample-app/src/users/entities/user.entity.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { IsEmail, IsInt, IsPositive, IsString, MaxLength } from "class-validator";
+
+export class User {
+  @IsInt()
+  @IsPositive()
+  id: number;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MaxLength(255)
+  name: string;
+
+  @IsString()
+  hashedPassword: string;
+}
